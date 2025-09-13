25d3308c103433d9cfa9201ca6535c23e6f95338
Ujjwal <45ujjwal@gmail.com>
2025-09-13 22:42:26 +0530

    Auth App

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,63 @@
+import { user as User } from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { generateVerificationCode } from '../utils/generateVerificationCode.js';
+import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists"
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const verificationToken = generateVerificationCode();
+
+    const user = new User({
+      email,
+      password: hashPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    await sendVerificationEmail(user.email, verificationToken)
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined
+      }
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+
+export const login = async () => {
+
+}
+
+export const logout = async () => {
+  
+}
---
+++ backend/mailtrap/email.js
@@ -0,0 +1,22 @@
+import {mailtrapClient, sender} from './mailtrap.config.js';
+import { VERIFICATION_EMAIL_TEMPLATE } from './emailTemplate,js'
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+    const recipient = [{email}]
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "verify your email",
+            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
+            category: "Email Verification"
+        })
+        console.log("Email sent successfully", response);
+    } catch (error) {
+        throw new Error(`Error sending verification email: ${error}`);
+        console.log(`Erro sending verification`, error);
+        
+    }
+
+}
